64ec76f919dd6c60c829ec5cde0b971978d9b040
anilikarikatti <anilkarikatti333@gmail.com>
2023-03-13 14:57:34 +0530

    game complete

--- src/components/createDiv.js
+++ src/components/createDiv.js
@@ -4,43 +4,67 @@
 import { useState } from "react";
 
 
-function CreateDiv({ke,number}){ 
+function CreateDiv({ke,number,count,setWin,win,inc,red,setGameStatus}){ 
   
 
-
+  let [clicked,setClicked] = useState(false)
   let [num,setNum]=useState(number);
   let [redColor,SetRedColor]=useState(false);
+
+  
 
   if(num && !redColor){
     SetRedColor(true);
     setNum(0);
     setTimeout(redTowhite,2000)
+
   }
 
   function redTowhite(){
     SetRedColor(false);
+
   }
 
 
   return (
         <>  
-            <div key={ke} className={number?'red':'white'} onClick={((e)=>{ if(e.target.className == 'red'){
+            {/* {number ? <RedCount/>:''} */}
+            
+            <div key={ke} className={number?'red':'white'} onClick={win ?((e)=>{ if(e.target.className == 'red'){
               // console.log(e.target.className);
               SetRedColor(true);
+              if(number && !clicked){
+                inc()
+                setClicked(true)
+
+                console.log(count,red);
+                // if(red == coun){
+                //   console.log(coun);
+                //   setWin(false)
+                // }
+              }
+            }
+            else{
+              // setGameStatus(false)
+
+                setWin(false)
             }
             }
           
-  )} style={{
+          
+  ) : ""} style={{
                         height:'100px',
                         border:'2px solid black',
                         flex:'auto',
-                        backgroundColor:(redColor ?'red':'white')
+                        backgroundColor:(redColor ?'red':'white'),
                       }}></div>
 
-          
+                   
       </>
+                    
     )
+
   }
   
-  
+
   export default CreateDiv;
--- src/components/mainDiv.js
+++ src/components/mainDiv.js
@@ -1,36 +1,95 @@
 // import './App.css';
-import { useState} from 'react';
+import { useRef, useState} from 'react';
 import CreateDiv from './createDiv';
+
 
 
 function MainDiv(){
   
-
+  let ref = useRef(0);
+  let [gameStatus,setGameStatus] = useState(false)
+  let [win,setWin] = useState(true)
   let rows =[];
   for(let i=0;i<16;i++){
       rows.push(i);
   }
 
+
+  let rd_count = ()=>{
+    ref.current += 1
+  }
+
+
+  let count =0;
+
+  let inc = ()=>{
+    count+=1
+    // console.log(count);
+    // console.log(ref.current);
+    
+    if(ref.current == count){
+        setWin(false)
+        setGameStatus(true)
+    }
+  }
+
+
      return (
-      <>      <div className='main' style={{
+      <>     
+        {win ?
+        <div style={{backgroundColor:"#f5c5cb",height:'600px'}}> <h2 style={{textAlign:"center",textTransform:"capitalize"
+        
+        }}>memory game</h2>
+       <div className='main' style={{
         display:'grid',
         // flexDirection:'row'
         gridTemplateColumns:'3fr 3fr 3fr 3fr',
-  
+        
+        // height:'800px',
+        marginTop:'50px',
+        marginLeft:'100px',
+        marginRight:'100px'
+
+
+        
       }}>
           {
+
+
             rows.map(elem=>{
-              // console.log(colored);
+              // console.log("colored");
                let  number = Math.floor(Math.random()*10)%2;
 
-           return  <CreateDiv key ={elem} ke={elem}   number={number}/>   
+              if(number){
+                rd_count()
+              }
+
+
+           return  <CreateDiv key ={elem} ke={elem}   number={number} count={count} setWin={setWin} win={win} inc={inc}  red   = {ref.current} setGameStatus = {setGameStatus}/>   
           })
           }
 
-      </div>
+      </div></div>
+      : <div style={{
+        // textAlign:"center",
+        height:"350px",
+        backgroundColor:"#f5c5cb",
+        width:'500px',
+        marginLeft:'400px',
+        marginTop:'100px'
+
+
+
+      }}><h1 style={{position:"relative",
+      top:"140px",textAlign:"center" , textTransform:"capitalize" }}> {gameStatus ? "congratulations  " : "o sorry  "} you {gameStatus ? "win " : "loose "}  the game </h1>
+      <h3 style={{position:"relative",
+      top:"140px",textAlign:"center" }}><a href='/' style={{textDecoration:"none",color:"#D12F42"}}> play again</a></h3>
+      </div>}
       </>
 
     )
   }
   
+
+
   export default MainDiv;
